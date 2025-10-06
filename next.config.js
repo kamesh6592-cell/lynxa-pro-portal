a7073c56062cc6eb300fa75f305c432ccd6ced07
kamesh6592-cell <kamesh6592@gmail.com>
2025-10-06 20:26:27 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // You can add Next.js configurations here
+  // For example, to enable experimental features:
+  // experimental: {
+  //   appDir: true,
+  // },
+}
+
+module.exports = nextConfig
