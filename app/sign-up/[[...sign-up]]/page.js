f0ccb65b1ac10a01608195b7f99163080a99e8f2
kamesh6592-cell <kamesh6592@gmail.com>
2025-10-06 20:21:03 +0530

    Create page.js

---
+++ app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,10 @@
+// app/sign-up/[[...sign-up]]/page.js
+import { SignUp } from '@clerk/nextjs'
+
+export default function Page() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <SignUp path="/sign-up" />
+    </div>
+  )
+}
