3cdfffc33c9e943dc4b2a82ae1404789ca8c337e
kamesh6592-cell <kamesh6592@gmail.com>
2025-10-07 11:49:07 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,21 +1,28 @@
 // app/layout.js
-import { ClerkProvider } from '@clerk/nextjs'
-import { ThemeProvider } from './providers'
-import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs';
+import { ThemeProvider } from './providers';
+import './globals.css';
 
 export const metadata = {
   title: 'Lynxa Pro Portal',
   description: 'Manage your Lynxa Pro API Keys',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-        <html lang="en">
-          <body>{children}</body>
-        </html>
-      </ThemeProvider>
+      <html lang="en" suppressHydrationWarning>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </html>
     </ClerkProvider>
-  )
+  );
 }
